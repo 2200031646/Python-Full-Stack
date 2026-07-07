83200c68033a9f28b4ad8affd41086a1f6b7f42b
Abburi Yasaswini <143771961+2200031646@users.noreply.github.com>
2026-07-07 19:03:09 +0530

    Add files via upload

--- Conditions.py
+++ Conditions.py
@@ -157,6 +157,7 @@ b=int(input("value:"))
 if b in a:
     print("true")'''
 
+#if-else
 #if-else condition by using comparision operators
 
 '''a=4
@@ -392,5 +393,326 @@ if b in a:
     print("True")
 else:
     print("False")'''
+
+#if-elif-else
+#if-elif-else condition by using comparision operators
+
+'''a=3
+b=5
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")'''
+
+'''a=4
+b=7
+if a==b:
+    print("less")
+elif b<a:
+    print("greater")
+elif a!=b:
+    print("not equal")'''
+
+'''a=1
+b=6
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=3
+b=9
+if a==b:
+    print("less")
+elif b<a:
+    print("greater")
+else:
+    print("true")'''
+    
+#if-elif-else condition by using logical operators
+
+'''a=11
+b=25
+if a<b and b>a:
+    print("True")
+elif a>b and b<a:
+    print("False")
+else:
+    print("invalid")'''
+
+'''a=3
+b=6
+if a<=b and b>=a:
+    print("True")
+elif a>=b and b<=a:
+    print("False")
+else:
+    print("invalid")'''
+
+'''a=2
+b=7
+if a!=b and b==a:
+    print("True")
+elif b!=a and a==b:
+    print("False")
+else:
+    print("invalid")'''
+
+'''a=17
+b=19
+if a<b or b>a:
+    print("True")
+elif a>b or b<a:
+    print("False")
+else:
+    print("invalid")'''
+
+'''a=21
+b=52
+if a<=b or b>=a:
+    print("True")
+elif a>=b or b<=a:
+    print("False")
+else:
+    print("invalid")'''
+
+#All logical operators can be done in one code
+'''a=2
+b=25
+if a<b and b>a:
+    print("True")
+elif a!=b or a==b:
+    print("equal")
+elif not b>a:
+    print("false")
+else:
+    print("invalid")'''
+
+#if-elif-else condition by using identify operators
+
+'''a=22
+if type(a) is int:
+    print("it is int")
+elif type(a) is not int:
+    print("false")
+else:
+    print("invalid")'''
+    
+'''a=2.4
+if type(a) is float:
+    print("it is float")
+elif type(a) is not float:
+    print("false")
+else:
+    print("invalid")'''
+
+'''a="hello"
+if type(a) is str:
+    print("it is string")
+elif type(a) is not str:
+    print("false")
+else:
+    print("invalid")'''
+
+'''a=2+6j
+if type(a) is complex:
+    print("it is complex")
+elif type(a) is not complex:
+    print("false")
+else:
+    print("invalid")'''
+
+'''a=True
+if type(a) is bool:
+    print("it is boolean")
+elif type(a) is not bool:
+    print("false")
+else:
+    print("invalid")'''
+
+#if-elif-else condition by using membership operators
+
+'''a=10,12,14,16,18
+if 10 in a:
+    print("True")
+elif 12 not in a:
+    print("False")
+else:
+    print("invalid")'''
+
+#multiple-if conditions
+#multiple-if conditions by using comparision operators
+'''a=10
+b=40
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+'''a=12
+b=30
+if a==b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+'''a=1
+b=4
+if a==b:
+    print("less")
+if b>a:
+    print("greater")
+if a>=b:
+    print("not equal")
+else:
+    print("true")'''
+
+#multiple-if conditions by using logical operators
+
+'''a=21
+b=25
+if a<b and b>a:
+    print("True")
+if a!=b or a==b:
+    print("equal")
+if not b<a:
+    print("false")
+else:
+    print("invalid")'''
+
+#multiple-if conditions by using identify operators
+
+'''a=22
+if type(a) is int:
+    print("it is int")
+if type(a) is not int:
+    print("false")
+else:
+    print("true")'''
+
+'''a=2.3
+if type(a) is float:
+    print("it is float")
+if type(a) is not float:
+    print("false")
+else:
+    print("true")'''
+
+'''a="hi"
+if type(a) is str:
+    print("it is str")
+if type(a) is not str:
+    print("false")
+else:
+    print("true")'''
+
+'''a=2+2j
+if type(a) is complex:
+    print("it is complex")
+if type(a) is not complex:
+    print("false")
+else:
+    print("true")'''
+
+'''a=22
+if type(a) is bool:
+    print("it is bool")
+if type(a) is not bool:
+    print("false")
+else:
+    print("true")'''
+
+#multiple-if conditions by using membership operators
+
+'''a=10,12,14,16,18
+if 10 in a:
+    print("True")
+if 11 not in a:
+    print("False")
+else:
+    print("invalid")'''
+
+#nested-if
+#nested-if conditions
+
+'''a=7
+b=9
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+#no output as main condition fails
+'''a=6
+b=10
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a=17
+b=19
+if a!=b:
+    print("true")
+    if b==a:
+        print("greater")'''
+
+'''a=6
+b=12
+if a!=b:
+    print("true")
+    if b==a:
+        print("equal")
+    else:
+        print("false")'''
+
+'''a=4
+b=22
+if a==b:
+    print("true")
+    if b>a:
+        print("greater")
+else:
+    print("false")'''
+
+'''a=2
+b=3
+if a!=b:
+    print("true")
+    if b==a:
+        print("equal")
+    else:
+        print("false")
+else:
+    print("not true")'''
+
+'''a=14
+b=17
+if a!=b:
+    print("true")
+    if b==a:
+        print("equal")
+    elif a<b:
+        print("less")
+    else:
+        print("false")'''
+
+'''a=int(input())
+b=int(input())
+if a!=b:
+    print("true")
+    if b==a:
+        print("equal")
+    elif b>a:
+        print("greater")
+    else:
+        print("false")
+else:
+    print("program ends")'''
 
 
