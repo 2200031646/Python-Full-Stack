aca268ed4acd291684bcf4b3e11c481fb86b667e
Abburi Yasaswini <143771961+2200031646@users.noreply.github.com>
2026-07-09 19:52:59 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,108 @@
+#loops()
+#for,while,range,break,continue,pass
+#for loop()
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=" ")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=(5,6,7,4,9)
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a={5,6,7,8,9,10}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+b={"year":2026,"month":"july","date":9}
+'''for i in b:
+    print(i)
+print(type(b))
+print(type(i))
+for i in b.keys():
+    print(i)
+for i in b.values():
+    print(type(b))
+    print(type(i))
+    print(i)
+for i in b.items():
+    print(i)
+    print(type(b))
+    print(type(i))'''
+
+'''a="codegnan"
+for i in a:
+    print(i)'''
+
+'''b=[1.2,3.4,5.6]
+for i in b:
+    print(i)
+    print(type(b))
+    print(type(i))'''
+
+'''b=["c","python","java","c++"]
+for i in b:
+    print(i)
+    print(type(b))
+    print(type(i))'''
+    
+'''b=[4+9j,2+8j]
+for i in b:
+    print(i)
+    print(type(b))
+    print(type(i))'''
+
+'''b=[True,False]
+for i in b:
+    print(i)
+    print(type(b))
+    print(type(i))'''
+
+#task
+#["APPLE","BANANA","MANGO"]
+#print(fruits.upper())
+'''fruits = ["apple","banana","mango"]
+b=str(fruits)
+print(b.upper())'''
+
+'''fruits = ["apple","banana","mango"]
+for i in fruits:
+    print(i.upper(),end=" ")'''
+
+'''fruits = ["apple","banana","mango"]
+b=[]
+for i in fruits:
+    b.append(i.upper())
+print(b)'''
+
+
+#a=[1,3,5,7,9,"code"]
+#[1,3,5,7,9,"c","o","d","e"]
+
+'''a=[1,3,5,7,9,"code"]
+a.extend("code")
+print(a)'''
+
+
+
+
+
+
+    
--- conditions_tasks.py
+++ conditions_tasks.py
@@ -4,22 +4,18 @@ if age>=18:
     print("Eligible for voting")
 else:
     print("Not eligible for voting")'''
-
 #even or odd
 '''num = int(input())
 if num%2==0:
     print("it is even")
 else:
     print("it is odd")'''
-
 #leap year or not
 '''year = int(input())
 if year%4==0:
     print("it is leap year")
 else:
     print("it is not leap year")'''
-
-
 #guest code
 '''name = "yasaswini"
 name1 = input("enter the name:")
@@ -33,7 +29,6 @@ if name=="yasaswini":
     print("Welcome",name)
 else:
     print("Welcome guest")'''
-
 #multiple users
 '''names = ["yashu","siri","sindhu","kavya","hari"]
 a = input("enter the name:")
@@ -41,7 +36,6 @@ if a in names:
     print("Welcome",a)
 else:
     print("Welcome guest")'''
-
 #vowels
 '''vowel = ["a","e","i","o","u"]
 b = input("enter the letter:").lower()
@@ -49,7 +43,6 @@ if b in vowel:
     print("It is vowel")
 else:
     print("it is consonent")'''
-
 #social-media-login
 #use only nested if
 '''username = input("enter the username:")
@@ -60,7 +53,6 @@ if username == password:
         print("Invalid credentials")
 else:
     print("Invalid credentials")'''
-
 
 '''username = input("enter the username:")
 password = int(input("enter the password:"))
@@ -83,7 +75,6 @@ if username == a:
         print("Invalid password")
 else:
     print("Invalid username")'''
-
 #using if-else
 '''username = input("enter the username:")
 password = input("enter the password:")
@@ -98,7 +89,6 @@ if username == "yasaswini" and password == 1234:
     print("Login successful")
 else:
     print("Invalid credentials")'''
-
 #multiple if
 
 '''age = int(input("enter age:"))
@@ -111,7 +101,48 @@ if marks>=80:
 if attendence>=70:
     print("Allow to write exams")'''
 
-    
+#if-elif-else
+#Bakery
+'''cake = int(input("enter the price:"))
+if cake == 1200:
+    print("redvelvet cake")
+elif cake == 1000:
+    print("almond cake")
+elif cake == 800:
+    print("chocolate cake")
+elif cake == 600:
+    print("butter scotch cake")
+else:
+    print("cake is not available")'''
+
+#Pizza
+'''pizza = input("enter the name:")
+if pizza == "bbq pizza":
+    print("Price is 800")
+elif pizza == "crispy chicken pizza":
+    print("prince is 600")
+elif pizza == "paneer pizza":
+    print("prince is 400")
+elif pizza == "corn pizza":
+    print("prince is 200")
+elif pizza == "frenchfries & coke":
+    print("price is 150")
+else:
+    print("not available")'''
+
+'''pizza = input("enter the name:")
+if pizza == "bbq pizza":
+    print(800)
+elif pizza == "crispy chicken pizza":
+    print(600)
+elif pizza == "paneer pizza":
+    print(400)
+elif pizza == "corn pizza":
+    print(200)
+elif pizza == "frenchfries & coke":
+    print(150)
+else:
+    print("not available")'''
 
 
 
@@ -130,5 +161,13 @@ if attendence>=70:
 
 
 
-    
+
+
+
+
+
+
+
+
+
 
