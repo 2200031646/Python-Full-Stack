4db8bb28268b4bb9d85c13d5b25ecf712bb83f40
Abburi Yasaswini <143771961+2200031646@users.noreply.github.com>
2026-07-10 14:34:20 +0530

    Add files via upload

--- Loops.py
+++ Loops.py
@@ -100,9 +100,115 @@ print(b)'''
 a.extend("code")
 print(a)'''
 
+#while loop()
+'''a=5
+while a>1:
+    print(a)'''
+
+'''a=5
+while a<1:
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)
+    a=a-1'''
+
+'''a=10
+while a>=1:
+    print(a)
+    a=a-1'''
+
+'''a=20
+while a>3:
+    print(a)
+    a=a-1'''
+
+'''a=20
+while a>3:
+    a=a-1
+    print(a)'''
+
+'''a=20
+while a>3:
+    a=a-1
+print(a)'''
+
+'''a=40
+while a>5:
+    a=a-1
+print(a)'''
+
+'''a=30
+while a>1:
+    print(a)
+    a+=1'''
+
+'''a=10
+while a>2:
+    print(a)
+    a-=1'''
+
+'''a=30
+while a>1:
+    print(a)
+    a-=1'''
+
+'''a=1
+while a<25:
+    print(a)
+    a+=1'''
+#voting
+'''while True:
+    age = int(input("Enter the age:"))
+    if age>=18:
+        print("Eligible for voting")
+    else:
+        print("Not eligible for voting")'''
+#bakery
+'''while True:
+    cake = int(input("enter the price:"))
+    if cake == 1200:
+        print("redvelvet cake")
+    elif cake == 1000:
+        print("almond cake")
+    elif cake == 800:
+        print("chocolate cake")
+    elif cake == 600:
+        print("butter scotch cake")
+    else:
+        print("cake is not available")'''
+#range()
+#start-stop-step
+'''for i in range(20):
+    print(i)'''
+
+'''for i in range(11,26):
+    print(i)'''
+#task
+'''for i in range(0,30,3):
+    print(i,end=" ")'''
+
+'''for i in range(0,50,5):
+    print(i,end=" ")'''
+
+'''for i in range(0,20,2):
+    print(i,end=" ")'''
+#grades  
+'''while True:
+    marks = int(input("Enter the marks:"))
+    if marks in range(91,101):
+        print("Grade-A")
+    elif marks in range(81,91):
+        print("Grade-B")
+    elif marks in range(70,81):
+        print("Grade-C")
+    elif marks in range(50,71):
+        print("Grade-D")
+    else:
+        print("Fail,study well....")'''
 
 
 
 
 
-    
