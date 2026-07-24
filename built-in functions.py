6938e45ecfb9139cc43ad4aa2226732db4e7df34
Abburi Yasaswini <143771961+2200031646@users.noreply.github.com>
2026-07-24 20:07:11 +0530

    Add files via upload

--- built-in functions.py
+++ built-in functions.py
@@ -206,13 +206,83 @@ print(type(d))'''
 b=list(filter(None,a))
 print(b)'''
 
+#map()->each object from a collection and forms a new collection
+#max(),min(),sum()
+'''print(max(8,7,5,9,10,20))
 
+print(min(8,7,5,9,10,20))
 
+a=1,2,3,4,6,10
+print(sum(a))
 
+print(sum([5,6,7,8,9]))'''
 
+'''a=[2,5,7,9,11,12,14,16,19,20]
+b=[1,3,5,7,10,15,12,21,23,25]
+c=list(map(max,a,b))
+print(c)
+c=list(map(min,a,b))
+print(c)'''
+# String Input
+'''a=input("data1:")
+b=input("data2:")
+print(a+b)'''
 
+'''a,b=input("enter the names").split(",")
+print(a+b)'''
 
+'''a,b=[x for x in input("Enter the name:").split(",")]
+print(a+b)'''
 
+'''a,b=map(str,input("enter the name:").split(","))
+print(a+b)'''
+# Integer Input
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+'''a,b=[int(x) for x in input("enter the values:").split(",")]
+print(a+b)'''
+
+'''a,b=int(input()).split(",")
+print(a+b)'''#error
+
+'''a,b=map(int,input("enter the values:").split(","))
+print(a+b)'''
+# Using map() with Different Collections
+'''a=list(map(int,input("enter the values:").split(",")))
+print(a)
+print(type(a))'''
+
+'''a=tuple(map(int,input("enter the values:").split(",")))
+print(a)
+print(type(a))'''
+
+'''a=set(map(int,input("enter the values:").split(",")))
+print(a)
+print(type(a))'''
+
+'''a=list(map(eval,input("enter the values:").split(",")))
+print(a)
+print(type(a))'''
+# Task - Dictionary Creation
+'''a=input("enter the key and value:")
+b=dict(i.split(":") for i in a.split(","))
+print(b)'''
+# Mini Project - Marks Analysis Report
+'''students = int(input("Enter the no of students:"))
+marks=[]
+for i in range(1,students+1):
+    mark=int(input(f"student {i} marks: "))
+    marks.append(mark)
+for i in marks:
+    print(i)
+print("...........Marks Analysis Report............")
+print("Total number of students:",students)
+print("Highest marks:",max(marks))
+print("Lowest marks:",min(marks))
+print("Total marks:",sum(marks))
+print("Average:",sum(marks)/students)'''
 
 
 
