9f58fc85f19f9a503a426d12d1a400d4ac2333ef
Abburi Yasaswini <143771961+2200031646@users.noreply.github.com>
2026-07-25 18:33:10 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,18 @@
+# Importing a Module
+'''import mymodule
+mymodule.greetings("Yasaswini")'''
+
+# Testing Different Module Examples
+'''import mymodule
+
+import mymodule'''
+
+# Accessing a Dictionary from a Module
+'''import mymodule
+b=mymodule.details["idnos"]
+c=mymodule.details["names"]
+print(b)
+print(c)'''
+
+#Checking Script and Module
+'''import mymodule'''
---
+++ mymodule.py
@@ -0,0 +1,35 @@
+# Creating a Function in a Module
+'''def greetings(name):
+    print("Welcome",name)'''
+# Module Example - Arithmetic
+'''a=4
+b=8
+print(a+b)'''
+# Module Example - User Input
+'''a=int(input("a value:"))
+b=int(input("b value:"))
+print(a+b)'''
+
+# Storing a Dictionary in a Module
+'''details={"idnos":[10,20,30],
+         "names":["yashu","ram","sam"],
+         "marks":[60,70,80]}'''
+
+
+'''if __name__=="__main__":
+    a=[10,20,30,40,50]
+    a.append("code")
+    a.extend("code")
+    print(a)'''
+
+
+# Checking Script and Module
+'''def dummy():
+    if __name__=="__main__":
+        print("This program is run as script")
+    else:
+        print("This program is run as module")
+dummy()'''
+        
+
+
