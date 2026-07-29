9d42d0886b383b74d39efac899bea6069270599a
Abburi Yasaswini <143771961+2200031646@users.noreply.github.com>
2026-07-29 19:30:56 +0530

    Add files via upload

--- different_modules.py
+++ different_modules.py
@@ -138,15 +138,84 @@ if a<b:
 '''a=10
 b=20
 if a>b:
-    print("less")'''    
-    
+    print("less")'''
+# Exception Handling
+#try->Instructions from which we are expecting the exceptions
+#except->exceptions are raised in try block it will be handle by this block
+#else->optional(no exceptions)
+#finally->always it will display
+'''while True:
+    try:
+        a=int(input("a value:"))
+        b=int(input("b value:"))
+        c=a//b
+        print(c)
+    except:
+        print("exception is raised")
+    else:
+        print("no exceptions")
+    finally:
+        print("program ends....")'''
+#regex(regular expressions)
+'''a="codegnan is in vijayawada"
+print(a)'''
 
+'''a="codegnan\nis\tin\nvijayawada"
+print(a)'''       
 
+# Raw String (r"")
+'''a=r"codegnan\nis\tin\nvij"
+print(a)'''
+#compile(),search(),findall(),split(),sub()
 
+#sequence characters
+#\w->it matches alphanumeric
+#\W->it matches non-alpha numeric
+#\d->it matches any digit
+#\D->it matches non-digit
+#\s->it represents white spaces
+#\S->it represents non-white spaces
 
+# compile()
+'''import re
+a = "mat cat maths money cash code cup dog donkey mug"
+b = re.compile(r"m\w\w\w\w")
+print(b)'''
+# search()
+'''c = b.search(a)
+print(c)'''
 
+'''b = re.search(r"m\w+", a)
+print(b)'''
+# findall()
+'''c = re.findall(r"c\w+",a)
+print(*c)'''
+#split()
+'''d=re.split(r"m",a)
+print(d)
 
+e=re.split(r"\S",a)
+print(e)
 
+#sub()
+f=re.sub("m","a",a)
+print(f)'''
+# Regex Task
+'''import re
+a="year 2026 month 7 date 29"
+b=re.findall(r"\d+",a)
+print(b)
+c=re.findall(r"\D+",a)
+print(c)'''
+
+'''import re
+e="code dog donkey"'''
+# Matching Words Containing 'd'
+'''f=re.findall(r"d\w+",e)
+print(f)'''
+# Matching Words Starting with 'do' Using Word Boundary
+'''g=re.findall(r"\bdo\w+",e)
+print(g)'''
 
 
 
